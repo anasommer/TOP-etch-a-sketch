3b3fabebf9dff4932018487e5812b2cf25d8885d
Anastassia Sommer <anastassia.sommer@gmail.com>
2024-03-08 09:23:18 +0100

    init commit and plan

---
+++ script.js
@@ -0,0 +1,9 @@
+// create divs using JS inside a container, grid 16 x 16
+// have a fixed with, ex. 960 px for the grid
+// use flexbox to make divs appear as a grid
+// be careful of borders and margins
+// set a hover effect so the divs change color when mouse passes over them, leaving a pixelated trail through the grid (add a new class to the div which will change its background)
+// add a button to the top of the screen
+// when button is clicked, ask user with a popup the number of squares per side for the new grid (set limit of max 100)
+// when answer is provided, existing grid shoul be removed
+// new grid should be generated
