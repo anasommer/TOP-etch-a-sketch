11f3c6bdbdccb2390fcec87ddeb2d607a248765e
Anastassia Sommer <anastassia.sommer@gmail.com>
2024-03-08 19:57:16 +0100

    done

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
-// create divs using JS inside a container, grid 16 x 16
-// have a fixed width, ex. 960 px for the grid
-// use flexbox to make divs appear as a grid
-// be careful of borders and margins
-
 // set a hover effect so the divs change color when mouse passes over them, leaving a pixelated trail through the grid (add a new class to the div which will change its background)
-// add a button to the top of the screen
-// when button is clicked, ask user with a popup the number of squares per side for the new grid (set limit of max 100)
-// when answer is provided, existing grid shoul be removed
-// new grid should be generated
 
 const containerEl = document.querySelector('.container');
+const btn = document.querySelector('#btn');
+const gridItems = document.querySelectorAll('.grid-item');
 
 function drawGrid(num) {
   let size = 960 / num;
@@ -19,9 +12,24 @@ function drawGrid(num) {
       gridItem.style.width = `${size}px`;
       gridItem.style.height = `${size}px`;
       gridItem.classList.add('grid-item');
+      gridItem.addEventListener('mouseover', () => {
+        gridItem.classList.add('draw');
+      });
 
       containerEl.append(gridItem);
     }
   }
 }
+
+btn.addEventListener('click', () => {
+  const num = prompt('How many squares to draw? (max 100)');
+
+  if (!Number(num)) {
+    prompt('Please, enter a number');
+  }
+
+  containerEl.innerHTML = '';
+  drawGrid(Math.floor(num));
+});
+
 drawGrid(16);
